533273f2ce0ccfbb946554a76d1a1f42c6ad5182
Villy Rosa <villyrosa@hotmail.com>
2022-03-29 02:41:33 -0300

    Adição das aulas 1 a 8 | 29.03.2022

---
+++ Aulas 7 e 8/PongLy/PongLy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("PongLy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PongLy")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("6bcc385d-e0a1-4daa-a0f2-0333a3a45aa4")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Aulas de 1 a 6/MeuJogo/MeuJogo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(f(2) + "\n");
+            deixarLike();
+        }
+
+        static int f(int x)
+        {
+            return x + 1;
+        }
+        static void deixarLike()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Você deixou o " + (i + 1) + "º like!\n");
+            }
+        }
+    }
+}
